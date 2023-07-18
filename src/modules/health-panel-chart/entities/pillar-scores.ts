2ab26153b707c4dec9062dcc42bff50a1476021d
Alexandro Hervis <alexandro.hervis@db1.com.br>
2023-07-18 19:55:07 -0300

    ajustando valores para saber se o pilar esta saudavel ou nao

--- src/modules/health-panel-chart/entities/pillar-scores.ts
+++ src/modules/health-panel-chart/entities/pillar-scores.ts
@@ -10,6 +10,15 @@ export class PillarScores implements IPillarScores {
   }
 
   get isHealthy(): boolean {
-    return this.score >= 80;
+    const healthyRanges: any = {
+      'Gestão de testes': 30,
+      'Gestão de Build & Release': 20,
+      'Monitoramento de Aplicações': 10,
+      'Qualidade do Código': 5,
+      'Infraestrutura como Código': 30,
+    };
+    const currentPillarName = this.name as any;
+    const healtyRange = healthyRanges[currentPillarName];
+    return this.score >= healtyRange;
   }
 }
