7e0b80c84ab819b7d458c27d549551f9dbfe421e
Alexandro Hervis <alexandro.hervis@db1.com.br>
2023-12-08 11:35:45 -0300

    Fix: Corrigindo arquivos de configuracao

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
     port: 5385,
     proxy: {
       '/api': {
-        target: 'https://localhost:7101',
+        target:
+          process.env.VITE_PROXY_REMOTE_URL ||
+          'https://api-healthpanel.db1.com.br',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
